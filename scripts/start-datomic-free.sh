fd23ee04f243c9e53ba30d7bd15ddca92c5396cf
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-05-09 20:37:03 +0200

    added a script to start the transactor

---
+++ scripts/start-datomic-free.sh
@@ -0,0 +1,11 @@
+# Process the property files to ensure that the transactor listens on the port provided by Heroku
+
+PROPERTIES=~/datomic/config/samples/free-transactor-template.properties
+
+sed "s/^port=4334/port=$PORT/" ${PROPERTIES} > ${PROPERTIES}.tmp
+
+mv ${PROPERTIES}.tmp ${PROPERTIES}
+
+transactor ${PROPERTIES}
+
+
